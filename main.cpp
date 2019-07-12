5c53398a28ad28b228b91aedfa8d19c4aff5eaaf
maxs-im <maxim1998g@gmail.com>
2019-07-12 21:16:18 +0300

    Using a timer asynchronously

--- main.cpp
+++ main.cpp
@@ -2,13 +2,21 @@
 #include <boost/asio.hpp>
 #include <boost/date_time/posix_time/posix_time.hpp>
 
+void print(const boost::system::error_code& /*e*/);
+
 int main() {
     boost::asio::io_service io;
     boost::asio::deadline_timer t(io, boost::posix_time::seconds(5));
 
-    t.wait();
-
-    std::cout << "Hello, world!" << std::endl;
+    //t.wait();
+    t.async_wait(&print);
+    std::cout << "Main thread\n";
+    io.run();
 
     return 0;
+}
+
+void print(const boost::system::error_code& /*e*/)
+{
+    std::cout << "Async thread\n";
 }
