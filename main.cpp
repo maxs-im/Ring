6db8efc36b07c183b96a5b801dc8dcd3e8f0b9d4
maxs-im <maxim1998g@gmail.com>
2019-07-12 21:33:47 +0300

    Binding arguments to a handler

--- main.cpp
+++ main.cpp
@@ -1,22 +1,36 @@
 #include <iostream>
 #include <boost/asio.hpp>
+#include <boost/bind.hpp>
 #include <boost/date_time/posix_time/posix_time.hpp>
 
-void print(const boost::system::error_code& /*e*/);
+void print(const boost::system::error_code& /*e*/,
+        boost::asio::deadline_timer* t, int* count);
 
 int main() {
     boost::asio::io_service io;
-    boost::asio::deadline_timer t(io, boost::posix_time::seconds(5));
+    boost::asio::deadline_timer t(io, boost::posix_time::seconds(1));
 
     //t.wait();
-    t.async_wait(&print);
-    std::cout << "Main thread\n";
+    //t.async_wait(&print);
+    int count = 0;
+    t.async_wait(boost::bind(print,
+                             boost::asio::placeholders::error, &t, &count));
+
     io.run();
+
+    std::cout << "Final count is " << count << std::endl;
 
     return 0;
 }
 
-void print(const boost::system::error_code& /*e*/)
+void print(const boost::system::error_code& /*e*/,
+           boost::asio::deadline_timer* t, int* count)
 {
-    std::cout << "Async thread\n";
+    if (*count < 5) {
+        std::cout << *count << std::endl;
+        ++(*count);
+        t->expires_at(t->expires_at() + boost::posix_time::seconds(1));
+        t->async_wait(boost::bind(print,
+                boost::asio::placeholders::error, t, count));
+    }
 }
