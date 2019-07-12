b64c1592a12b5c88ae7b38ae7dfab961a219a761
maxs-im <maxim1998g@gmail.com>
2019-07-12 21:47:24 +0300

    Using a member function as a handler

--- main.cpp
+++ main.cpp
@@ -3,34 +3,37 @@
 #include <boost/bind.hpp>
 #include <boost/date_time/posix_time/posix_time.hpp>
 
-void print(const boost::system::error_code& /*e*/,
-        boost::asio::deadline_timer* t, int* count);
+class printer {
+public:
+    printer(boost::asio::io_service& io)
+            : timer_(io, boost::posix_time::seconds(1)),
+              count_(0)
+    {
+        timer_.async_wait(boost::bind(&printer::print, this));
+    }
+
+    void print() {
+        if (count_ < 5) {
+            std::cout << count_ << std::endl;
+            ++count_;
+
+            timer_.expires_at(timer_.expires_at() + boost::posix_time::seconds(1));
+            timer_.async_wait(boost::bind(&printer::print, this));
+        }
+    }
+
+    ~printer() {
+        std::cout << "Final count is " << count_ << std::endl;
+    }
+private:
+    boost::asio::deadline_timer timer_;
+    int count_;
+};
 
 int main() {
     boost::asio::io_service io;
-    boost::asio::deadline_timer t(io, boost::posix_time::seconds(1));
-
-    //t.wait();
-    //t.async_wait(&print);
-    int count = 0;
-    t.async_wait(boost::bind(print,
-                             boost::asio::placeholders::error, &t, &count));
-
+    printer p(io);
     io.run();
 
-    std::cout << "Final count is " << count << std::endl;
-
     return 0;
-}
-
-void print(const boost::system::error_code& /*e*/,
-           boost::asio::deadline_timer* t, int* count)
-{
-    if (*count < 5) {
-        std::cout << *count << std::endl;
-        ++(*count);
-        t->expires_at(t->expires_at() + boost::posix_time::seconds(1));
-        t->async_wait(boost::bind(print,
-                boost::asio::placeholders::error, t, count));
-    }
 }
