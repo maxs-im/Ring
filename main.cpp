f1f58ef224273c8e3e8440b211ce04d76cbcd4be
maxs-im <maxim1998g@gmail.com>
2019-07-14 23:25:36 +0300

    A synchronous TCP daytime client

--- main.cpp
+++ main.cpp
@@ -1,66 +1,47 @@
 #include <iostream>
+#include <boost/array.hpp>
 #include <boost/asio.hpp>
-#include <boost/bind.hpp>
-#include <boost/thread/thread.hpp>
-#include <boost/date_time/posix_time/posix_time.hpp>
 
-class printer {
-public:
-    printer(boost::asio::io_service& io)
-            : timer_1(io, boost::posix_time::seconds(1)),
-              timer_2(io, boost::posix_time::seconds(1)),
-              strand_(io),
-              count_(0)
+using boost::asio::ip::tcp;
+
+int main(int argc, char* argv[])
+{
+    try
     {
-        timer_1.async_wait(strand_.wrap(boost::bind(&printer::print1, this)));
-
-        timer_2.async_wait(strand_.wrap(boost::bind(&printer::print2, this)));
-    }
-
-    void print1()
-    {
-        if (count_ < 10)
+        if (argc != 2)
         {
-            std::cout << "Timer 1: " << count_ << std::endl;
-            ++count_;
+            std::cerr << "Usage: client <host>" << std::endl;
+            return 1;
+        }
 
-            timer_1.expires_at(timer_1.expires_at() + boost::posix_time::seconds(1));
+        boost::asio::io_service io_service;
 
-            timer_1.async_wait(strand_.wrap(boost::bind(&printer::print1, this)));
+        tcp::resolver resolver(io_service);
+        tcp::resolver::query query(argv[1], "daytime");
+        tcp::resolver::iterator endpoint_iterator = resolver.resolve(query);
+
+        tcp::socket socket(io_service);
+        boost::asio::connect(socket, endpoint_iterator);
+
+        for (;;)
+        {
+            boost::array<char, 128> buf;
+            boost::system::error_code error;
+
+            size_t len = socket.read_some(boost::asio::buffer(buf), error);
+
+            if (error == boost::asio::error::eof)
+                break; // Connection closed cleanly by peer.
+            else if (error)
+                throw boost::system::system_error(error); // Some other error.
+
+            std::cout.write(buf.data(), len);
         }
     }
-
-    void print2()
+    catch (std::exception& e)
     {
-        if (count_ < 10)
-        {
-            std::cout << "Timer 2: " << count_ << std::endl;
-            ++count_;
-
-            timer_2.expires_at(timer_2.expires_at() + boost::posix_time::seconds(1));
-
-            timer_2.async_wait(strand_.wrap(boost::bind(&printer::print2, this)));
-        }
+        std::cerr << e.what() << std::endl;
     }
-
-    ~printer()
-    {
-        std::cout << "Final count is " << count_ << std::endl;
-    }
-private:
-    boost::asio::io_service::strand strand_;
-    boost::asio::deadline_timer timer_1;
-    boost::asio::deadline_timer timer_2;
-    int count_;
-};
-
-int main() {
-    boost::asio::io_service io;
-    printer p(io);
-
-    boost::thread t(boost::bind(&boost::asio::io_service::run, &io));
-    io.run();
-    t.join();
 
     return 0;
 }
