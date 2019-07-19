9ae63ed91b728841530f6aebd020397583c4a3f1
maxs-im <maxim1998g@gmail.com>
2019-07-19 03:07:19 +0300

    Client skeleton

--- client.cpp
+++ client.cpp
@@ -1,7 +1,67 @@
 #include <iostream>
+#include <thread>
 #include <boost/asio.hpp>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+using boost::asio::ip::tcp;
+
+class chat_client
+{
+public:
+    chat_client(boost::asio::io_service& io_service,
+                tcp::resolver::iterator endpoint_iterator)
+            : io_service_(io_service),
+              socket_(io_service)
+    {
+        do_connect(endpoint_iterator);
+    }
+
+    void close()
+    {
+        io_service_.post([this]() { socket_.close(); });
+    }
+private:
+    void do_connect(tcp::resolver::iterator endpoint_iterator)
+    {
+        boost::asio::async_connect(socket_, endpoint_iterator,
+                [](boost::system::error_code ec, tcp::resolver::iterator)
+                {
+                    if (!ec)
+                    {
+                        // TODO: read logic
+                    }
+                });
+    }
+
+    boost::asio::io_service& io_service_;
+    tcp::socket socket_;
+};
+
+int main(int argc, char* argv[]) {
+    try
+    {
+        if (argc != 3)
+        {
+            std::cerr << "Usage: chat_client <host> <port>\n";
+            return 1;
+        }
+
+        boost::asio::io_service io_service;
+
+        tcp::resolver resolver(io_service);
+        auto endpoint_iterator = resolver.resolve({ argv[1], argv[2] });
+        chat_client c(io_service, endpoint_iterator);
+
+        std::thread t([&io_service](){ io_service.run(); });
+
+        // TODO: logic for input reading
+
+        c.close();
+        t.join();
+    }
+    catch (std::exception& e)
+    {
+        std::cerr << "Exception: " << e.what() << "\n";
+    }
+
     return 0;
 }
