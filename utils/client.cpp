b985aa25ffac1c3f03e8700c4aec87634d454109
maxs-im <maxim1998g@gmail.com>
2019-07-22 00:35:18 +0300

    Split and rename client part
    Move credentials logic inside

--- utils/client.cpp
+++ utils/client.cpp
@@ -1,5 +1,127 @@
 //
-// Created by maxs on 21/07/19.
+// Client part of chat realization
 //
 
 #include "client.hpp"
+
+#include <iostream>
+
+using boost::asio::ip::tcp;
+
+ChatClient::ChatClient(boost::asio::io_service& io_service,
+            tcp::resolver::iterator endpoint_iterator)
+        : io_service_(io_service),
+          socket_(io_service)
+{
+    do_connect(std::move(endpoint_iterator));
+}
+
+Notification ChatClient::credentials2ntf(const std::string& login,
+        const std::string& password)
+{
+    return {login, NTFCommand::PASSWORD, password};
+}
+
+
+void ChatClient::write(const Notification& ntf) {
+    io_service_.post(
+            [this, ntf]()
+            {
+                bool write_in_progress = !ntfs_write.empty();
+                ntfs_write.push_back(ntf);
+                if (!write_in_progress)
+                {
+                    do_write();
+                }
+            });
+}
+
+void ChatClient::close()
+{
+    io_service_.post([this]() { socket_.close(); });
+}
+
+void ChatClient::do_connect(tcp::resolver::iterator endpoint_iterator)
+{
+    boost::asio::async_connect(socket_, std::move(endpoint_iterator),
+               [this](boost::system::error_code ec, tcp::resolver::iterator)
+               {
+                   if (!ec)
+                   {
+                       read_notifications();
+                   }
+               });
+}
+
+void ChatClient::read_notifications() {
+    boost::asio::async_read_until(socket_, buffer_, Notification::DELIMITER,
+              [this](boost::system::error_code ec, std::size_t /*length*/)
+              {
+                  if (!ec)
+                  {
+                      // all (disconnecting) notification logic provided here
+                      bool leave;
+                      try {
+                          std::istream is(&buffer_);
+                          std::string str;
+                          std::getline(is, str);
+
+                          ntf.update(str);
+                          leave = ntf.get_command() == NTFCommand::KICK;
+                          console::print_notification(
+                                  NTFCommand::decode_notification(ntf));
+                      }
+                      catch (...) {
+                          leave = true;
+                          console::print_notification("Smth goes wrong with receiving");
+                      }
+
+                      if (leave) {
+                          socket_.close();
+                      } else {
+                          read_notifications();
+                      }
+                  } else {
+                      console::print_notification("Bad data received");
+                      socket_.close();
+                  }
+              });
+}
+
+void ChatClient::do_write() {
+    auto new_message = ntfs_write.front().encode();
+    boost::asio::async_write(socket_,
+             boost::asio::buffer(new_message, new_message.size()),
+             [this](boost::system::error_code ec, std::size_t /*length*/)
+             {
+                 if (!ec)
+                 {
+                     ntfs_write.pop_front();
+                     if (!ntfs_write.empty())
+                     {
+                         do_write();
+                     }
+                 } else {
+                     console::print_notification("Smth goes wrong with sending");
+                     socket_.close();
+                 }
+             });
+}
+
+namespace console {
+    Notification read_credentials(std::string& login) {
+        std::string password;
+        std::cout << "Login: ";
+        while (login.empty()) std::getline(std::cin, login);
+
+        std::cout << "Password: ";
+        std::getline(std::cin, password);
+
+        return ChatClient::credentials2ntf(login, password);
+    }
+
+    void print_notification(std::string message) {
+        // TODO: prettify
+        std::cout << message << "\n";
+    }
+}
--- utils/client.hpp
+++ utils/client.hpp
@@ -1,14 +1,49 @@
 //
-// Created by maxs on 21/07/19.
+// Client part of chat
 //
 
 #ifndef RING_CLIENT_HPP
 #define RING_CLIENT_HPP
 
+#include "notification.hpp"
+#include <deque>
+#include <boost/asio.hpp>
 
-class client {
+// TODO: tmp, remove
+#include <iostream>
 
+class ChatClient
+{
+public:
+    ChatClient(boost::asio::io_service& io_service,
+                boost::asio::ip::tcp::resolver::iterator endpoint_iterator);
+
+    static Notification credentials2ntf(const std::string& login,
+            const std::string& password);
+
+    void write(const Notification& ntf);
+    void close();
+private:
+    void do_connect(boost::asio::ip::tcp::resolver::iterator endpoint_iterator);
+
+    // recursive async reading
+    void read_notifications();
+    // recursive async writing
+    void do_write();
+
+    // queue for writing
+    std::deque<Notification> ntfs_write;
+    // buffers for reading and storing current notification
+    boost::asio::streambuf buffer_;
+    Notification ntf;
+
+    boost::asio::io_service& io_service_;
+    boost::asio::ip::tcp::socket socket_;
 };
 
+// Utils for Client side
+namespace console {
+    void print_notification(std::string message);
+}
 
 #endif //RING_CLIENT_HPP
